3ede95a7cc8cce6a0342181628a62819f8abb3c8
kaoss08 <bryannolli01@gmail.com>
2026-05-03 20:07:19 +0200

    Update config.js

--- config.js
+++ config.js
@@ -20,11 +20,11 @@ global.wait = 'ⓘ 𝐂𝐚𝐫𝐢𝐜𝐚𝐦𝐞𝐧𝐭𝐨 ...';
 
 global.owner = [
   ['212779919974', 'kai', true],
-  ['xxxxxxxxxx'], // Numero, Nome, True (attenzione alla sintassi deve essere identico a sopra!)
+  ['xxxxxxxxxx'], 
   ['xxxxxxxxxx'],
   ['xxxxxxxxxx'],
   ['xxxxxxxxxx'],
-  ['xxxxxxxxxx']
+  ['xxxxxxxxxx'],
 ];
 
 
